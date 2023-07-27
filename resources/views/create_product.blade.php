266ab54b1ce7889ada8a8228c2a567812a79c261
waaizulfahmi <wandaaizulf@gmail.com>
2023-07-27 20:42:26 +0700

    added livewire

--- resources/views/create_product.blade.php
+++ resources/views/create_product.blade.php
@@ -7,16 +7,24 @@
     <title>Create Products</title>
 </head>
 <body>
+    @livewire('show-posts')
     <form action="{{route('store_product')}}" method="post" enctype="multipart/form-data"> 
         @csrf
         <input type="text" name="name" placeholder="name">
+        <br>
         <input type="text" name="description" placeholder="decs">
+        <br>
+
         <input type="number" name="price" placeholder="price">
+
+        <br>
         <input type="number" name="stock" placeholder="stock">
+
+        <br>
         <input type="file" name="image" placeholder="file">
+  
 
         <button type="submit">Submit</button>
-        
     </form>
 </body>
 </html>
